e24c9b127b009546c9832cebadfaa560353ce865
Eduard Perez <eduardgildardoperezfranco@gmail.com>
2026-01-07 09:12:42 -0500

    Update package-lock.json and lib files

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -1,12 +1,13 @@
 // Hooks Export
-export { useWindowSize } from './hooks';
-export { useClickOutside } from './hooks';
-export { useLocalStorage } from './hooks';
-export { useDarkMode } from './hooks';
-export { useIntersectionObserver } from './hooks';
-export { useAsync } from './hooks';
-export { useForm } from './hooks';
-export { useThrottle } from './hooks';
-export { useDebounce } from './hooks';
-export { useResizeObserver } from './hooks';
-export { useFocusTrap } from './hooks';
+// Note: These hooks are not currently implemented but are exported for future use
+// export { useWindowSize } from './hooks';
+// export { useClickOutside } from './hooks';
+// export { useLocalStorage } from './hooks';
+// export { useDarkMode } from './hooks';
+// export { useIntersectionObserver } from './hooks';
+// export { useAsync } from './hooks';
+// export { useForm } from './hooks';
+// export { useThrottle } from './hooks';
+// export { useDebounce } from './hooks';
+// export { useResizeObserver } from './hooks';
+// export { useFocusTrap } from './hooks';
